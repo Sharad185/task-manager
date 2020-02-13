92773fe54492e1f72817485d34954459bbf5ca75
Sharad kumar <ks7053898905@gmail.com>
2020-02-13 11:16:29 -0800

    change the mongodb connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://taskapp:Sharad@185!!@cluster0-rrdbm.mongodb.net/task-manager-api?retryWrites=true&w=majority',{useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false})
+mongoose.connect('mongodb+srv://taskapp:Sharad123!!@cluster0-rrdbm.mongodb.net/task-manager-api?retryWrites=true&w=majority',{useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false})
 
 
 // _v store the version of document
